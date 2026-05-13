c93fecdae3b5ba61506915a99aeee8bd09bc2683
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2026-05-12 21:01:11 -0300

    deleção de pasta dúplicada

--- api_pao_fresquim/src/main/java/com/pao_fresquim/api/Repositories/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pao_fresquim.api.Repositories;
-
-import com.pao_fresquim.api.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
